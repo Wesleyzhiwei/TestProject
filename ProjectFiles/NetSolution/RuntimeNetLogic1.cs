7d3c3833b0fed43897ea48d66ee126ad806ee6e8
wzz <zhao@rockwellautomation.com>
2023-04-25 16:03:06 +0800

    Add one of each

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,34 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.Recipe;
+using FTOptix.Store;
+using FTOptix.DataLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Report;
+using FTOptix.OPCUAServer;
+using FTOptix.CommunicationDriver;
+using FTOptix.Modbus;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
